6205f8df869d10ebbc987184ac740ef505691602
Etienne Dechamps <etienne@edechamps.fr>
2023-08-14 21:34:54 +0100

    Improve chart previous frames logic

--- generate-report.py
+++ generate-report.py
@@ -37,12 +37,25 @@ def generate_report():
     spec = json.load(args.spec_file)
     nominal_fps = spec["fps"]["num"] / spec["fps"]["den"]
     frame_duration = spec["fps"]["den"] / spec["fps"]["num"]
-    reference_frames = 1 * np.array(spec["frames"])
-    reference_transitions = pd.Series(
-        np.diff(reference_frames),
-        index=np.arange(1, reference_frames.size) * frame_duration,
+    reference_transitions = pd.DataFrame(
+        {"frame": spec["frames"]},
+        index=pd.Index(
+            np.arange(0, len(spec["frames"])) * frame_duration,
+            name="reference_timestamp_seconds",
+        ),
     )
-    reference_transitions = reference_transitions[reference_transitions != 0]
+    reference_transitions_diff = (
+        reference_transitions.loc[:, "frame"]
+        != reference_transitions.loc[:, "frame"].shift()
+    )
+    reference_transitions_diff[0] = False
+    reference_transitions.loc[:, "previous_frame_count"] = (
+        reference_transitions.groupby(reference_transitions_diff.cumsum())
+        .cumcount()
+        .shift()
+        + 1
+    )
+    reference_transitions = reference_transitions[reference_transitions_diff]
     reference_transitions_interval_seconds = interval(reference_transitions.index)
     print(
         f"Successfully loaded spec file containing {reference_transitions.size} frame transitions at {nominal_fps} FPS, with first transition at {reference_transitions_interval_seconds.left} seconds and last transition at {reference_transitions_interval_seconds.right} seconds for a total of {reference_transitions_interval_seconds.length} seconds",
@@ -53,13 +66,13 @@ def generate_report():
         sys.stdin,
         index_col="recording_timestamp_seconds",
         usecols=["recording_timestamp_seconds", "frame"],
-    ).squeeze()
+    )
     transitions_interval_seconds = interval(transitions.index)
     print(
-        f"Recording analysis contains {transitions.size} frame transitions, with first transition at {transitions_interval_seconds.left} seconds and last transition at {transitions_interval_seconds.right} seconds for a total of {transitions_interval_seconds.length} seconds",
+        f"Recording analysis contains {transitions.index.size} frame transitions, with first transition at {transitions_interval_seconds.left} seconds and last transition at {transitions_interval_seconds.right} seconds for a total of {transitions_interval_seconds.length} seconds",
         file=sys.stderr,
     )
-    if transitions.size == reference_transitions.size:
+    if transitions.index.size == reference_transitions.index.size:
         print("Number of recorded transitions matches the spec. Good.", file=sys.stderr)
     else:
         print(
@@ -69,9 +82,20 @@ def generate_report():
 
     # TODO: try harder to make this work when there are more or fewer
     # transitions than expected. reindex(method="nearest") might help.
-    transitions = transitions.to_frame()
+    transitions.reset_index(inplace=True)
+    transitions.index = reference_transitions.index
+    # TODO: check that the black/white frames match
+    transitions = pd.concat(
+        [
+            transitions,
+            reference_transitions.rename(
+                lambda column_name: "reference_" + column_name, axis="columns"
+            ),
+        ],
+        axis="columns",
+    )
     transitions.loc[:, "error_seconds"] = (
-        transitions.index - reference_transitions.index
+        transitions.loc[:, "recording_timestamp_seconds"] - transitions.index
     )
     linear_regression = np.polynomial.Polynomial.fit(
         transitions.index, transitions.loc[:, "error_seconds"], deg=1
@@ -89,22 +113,25 @@ def generate_report():
         )
     transitions.loc[:, "error_seconds"] -= linear_regression(transitions.index)
 
-    transitions.loc[:, "frame_and_last_duration"] = (
-        transitions.loc[:, "frame"]
-        + " after "
-        + np.insert(
-            np.round(np.diff(reference_transitions.index), 4) * 1000,
-            0,
-            [np.nan],
-        ).astype(str)
-        + " ms"
-    )
-
-    alt.Chart(transitions.reset_index()).mark_point().encode(
+    alt.Chart(transitions.reset_index()).transform_calculate(
+        label="Transition to "
+        + alt.expr.if_(alt.datum["reference_frame"], "white", "black")
+        + " (after "
+        + alt.datum["reference_previous_frame_count"]
+        + " "
+        + alt.expr.if_(alt.datum["reference_frame"], "black", "white")
+        + " frames)"
+    ).mark_point().encode(
         alt.X("recording_timestamp_seconds").scale(zero=False),
         alt.Y("error_seconds").scale(zero=False),
-        alt.Color("frame_and_last_duration"),
-    ).properties(width=1000, height=750).save(args.output_file)
+        alt.Color("label", type="nominal", title=None),
+    ).configure_legend(
+        labelLimit=0
+    ).properties(
+        width=1000, height=750
+    ).save(
+        args.output_file
+    )
 
 
 generate_report()
