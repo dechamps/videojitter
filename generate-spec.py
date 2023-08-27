211d211d6568b6d73f48fff599be336cf158555f
Etienne Dechamps <etienne@edechamps.fr>
2023-08-27 21:34:00 +0100

    Avoid consecutive repeats
    Two consecutive repeats basically cancel each other out for most intents
    and purposes, so ensure there is at least one transition between them.

--- generate-spec.py
+++ generate-spec.py
@@ -23,26 +23,56 @@ def parse_arguments():
     )
     argument_parser.add_argument(
         "--frame-repeat-ratio",
-        help="Ratio of transitions that will trigger a frame repeat",
+        help="Ratio of transitions that will have a repeated frame before or after the transition",
         default=0.1,
         type=float,
     )
     return argument_parser.parse_args()
 
 
+def nonconsecutive_integers_count(minimum, maximum):
+    """Returns the number of non-consecutive integers between `minimum` and
+    `maximum`, inclusive."""
+    return
+
+
+def nonconsecutive_random_integers(minimum, maximum, count):
+    """Generates an array of `count` integers between `minimum` and `maximum`.
+
+    The bounds are inclusive.
+
+    The distance between any two numbers in the output is guaranteed to be at
+    least 2 (e.g. the output can include 42 or 43, but not both).
+
+    `count` is clamped to the number of nonconsecutive integers that can fit
+    between `minimum` and `maximum`.
+    """
+    # Inspired by https://stackoverflow.com/a/31060350/172594
+    count = min(count, int((maximum - minimum + 2) / 2))
+    return (
+        np.sort(np.random.choice(maximum - minimum - count + 2, count, replace=False))
+        + np.arange(count)
+        + minimum
+    )
+
+
 def generate_frames(count, frame_repeat_ratio, rng):
     """Generate n frames alternating between black and white.
 
-    Some frames are repeated at random; however a given frame will never appear
-    more than twice in a row, and the first and last frames can never be part
-    of a repeat."""
-    transition_count = int(np.ceil(count / (1 + frame_repeat_ratio)))
-    transition_frame_counts = np.ones(transition_count, dtype=int)
+    Some frames are repeated at random, constrained by the following rules:
+     - A given frame will never appear more than twice in a row;
+     - Frames cannot be repeated on *both* sides of a transition (i.e. no
+       consecutive repeats);
+     - The first and last frames can never be repeated."""
+    transition_frame_counts = np.ones(
+        int(np.ceil(count / (1 + frame_repeat_ratio))), dtype=int
+    )
     transition_frame_counts[
-        rng.choice(
-            transition_frame_counts.size - 2, count - transition_count, replace=False
+        nonconsecutive_random_integers(
+            1,
+            transition_frame_counts.size - 2,
+            int(np.round(transition_frame_counts.size * frame_repeat_ratio)),
         )
-        + 1
     ] = 2
     return (
         ((np.arange(transition_frame_counts.size) + rng.choice([0, 1])) % 2)
@@ -53,6 +83,12 @@ def generate_frames(count, frame_repeat_ratio, rng):
 
 def generate_spec():
     args = parse_arguments()
+    # Frame repeat ratio cannot be higher than 50% as we don't allow consecutive
+    # repeats.
+    assert (
+        args.frame_repeat_ratio <= 0.5
+    ), "Frame repeat ratio cannot be higher than 0.5"
+
     frame_count = int(args.duration_seconds * (args.fps_num / args.fps_den))
     print(
         f"Generating {frame_count} frames at {args.fps_num / args.fps_den} FPS",
