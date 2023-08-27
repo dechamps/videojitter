f5c34ee9dc0e2de316f599c25429f1e9594320b8
Etienne Dechamps <etienne@edechamps.fr>
2023-08-27 18:52:05 +0100

    Add dummy audio

--- generate-video.py
+++ generate-video.py
@@ -34,32 +34,40 @@ def generate_video():
     args = parse_arguments()
     spec = json.load(sys.stdin)
     ffmpeg_process = (
-        ffmpeg.input(
-            "pipe:",
-            format="rawvideo",
-            pix_fmt="gray",
-            s="1x1",
-            r=f"{spec['fps']['num']}/{spec['fps']['den']}",
-        )
-        .filter(
-            "tpad",
-            start_duration=args.begin_padding,
-            stop_duration=args.end_padding,
-            start_mode="clone",
-            stop_mode="clone",
-        )
-        # TODO: add audio (important for video player clocking)
-        .output(
+        ffmpeg.output(
+            ffmpeg.input(
+                "pipe:",
+                format="rawvideo",
+                pix_fmt="gray",
+                s="1x1",
+                r=f"{spec['fps']['num']}/{spec['fps']['den']}",
+            ).filter(
+                "tpad",
+                start_duration=args.begin_padding,
+                stop_duration=args.end_padding,
+                start_mode="clone",
+                stop_mode="clone",
+            ),
+            # Include a dummy audio track as it makes the test video more
+            # realistic. Some video players (especially PC software) rely on the
+            # audio track for clocking, and will behave very differently if it's
+            # not there.
+            ffmpeg.input("anoisesrc=c=pink:r=48000:a=0.001", format="lavfi"),
             args.output_file,
             **{
-                "s": "hd1080",
-                "pix_fmt": "yuv420p",
+                "shortest": None,
                 "profile:v": "baseline",
                 "preset": "ultrafast",
+                # Make the video behave like typical HD video for compatibility
+                # and to ensure the video players behave similarly to a "real"
+                # video.
+                "s": "hd1080",
+                "pix_fmt": "yuv420p",
                 "color_primaries": "bt709",
                 "color_trc": "bt709",
                 "colorspace": "bt709",
                 "color_range": "tv",
+                "acodec": "ac3",
             },
         )
         .overwrite_output()
