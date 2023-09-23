9237f7996f0330f4ee47de96267b84ef71793e45
Etienne Dechamps <etienne@edechamps.fr>
2023-09-23 12:36:33 +0100

    Use slope at zero crossing to find edges

--- analyze-recording.py
+++ analyze-recording.py
@@ -31,9 +31,27 @@ def parse_arguments():
     )
     argument_parser.add_argument(
         "--min-edge-separation-seconds",
-        help="The minimum time interval between edges that the analyzer will be able to resolve. Note that this is the theoretical limit; in practice, this needs to include a safety margin of about 2x to account for the non-ideal response of the downsampling and upsampling filters. Higher values improve high frequency noise rejection but make it more likely the analyzer will fail to detect edges, and may slightly degrade timestamp resolution.",
+        help="The approximate minimum time interval between edges that the analyzer is expected to resolve. If the interval between two edges is shorter than this, the analyzer might become unable to precisely estimate timing differences between the two edges; if the interval is half as short, both edges may be missed entirely. On the other hand, setting this lower lets more high frequency noise in, possibly causing edge detection false positives.",
         type=float,
-        default=0.0005,
+        default=0.001,
+    )
+    argument_parser.add_argument(
+        "--max-edge-spread-seconds",
+        help='The approximate maximum amount of time an edge can spread over before the analyzer might fail to detect it. This determines how "sluggish" the system response (including the instrument) is allowed to be. Setting this higher will allow slower transitions to be detected, but lets more low frequency noise in, possibly causing faster transitions to be missed due to drift.',
+        type=float,
+        default=0.020,
+    )
+    argument_parser.add_argument(
+        "--min-edges-ratio",
+        help="The minimum number of edges that can be assumed to be present in the test signal, as a ratio of the number of transitions implied by the spec. Used in combination with --edge-slope-threshold.",
+        type=float,
+        default=0.25,
+    )
+    argument_parser.add_argument(
+        "--edge-slope-threshold",
+        help="The absolute slope-at-zero-crossing threshold above which an edge will be recorded, as a ratio of the Nth steepest slope, where N is dictated by --min-edges-ratio. Determines how sensitive the analyzer is when detecting edges.",
+        type=float,
+        default=0.3,
     )
     argument_parser.add_argument(
         "--boundaries-signal-frames",
@@ -48,38 +66,13 @@ def parse_arguments():
         default=0.5,
     )
     argument_parser.add_argument(
-        "--timestamp-resolution-seconds",
-        help="The desired resolution of the resulting transition timestamps, in seconds. This determines the upsampling ratio used before looking for edges. Higher values will reduce transition timing resolution but will make processing faster and less memory intensive.",
-        type=float,
-        default=0.00001,
+        "--output-downsampled-file",
+        help="(Only useful for debugging) Write the downsampled recording as a WAV file to the given path",
+        type=argparse.FileType(mode="wb"),
     )
     argument_parser.add_argument(
-        "--minimum-negative-slope-peak-height-ratio",
-        help="The minimum height required for a negative slope peak to be recorded as a falling edge, relative to the overall maximum negative slope.",
-        type=float,
-        default=0.3,
-    )
-    argument_parser.add_argument(
-        "--minimum-positive-slope-peak-height-ratio",
-        help="The minimum height required for a positive slope peak to be recorded as a rising edge, relative to the overall maximum negative slope.",
-        type=float,
-        default=0.3,
-    )
-    argument_parser.add_argument(
-        "--minimum-negative-slope-peak-prominence-ratio",
-        help="The minimum prominence required for a negative slope peak to be recorded as a falling edge, relative to the overall maximum negative slope.",
-        type=float,
-        default=0.3,
-    )
-    argument_parser.add_argument(
-        "--minimum-positive-slope-peak-prominence-ratio",
-        help="The minimum prominence required for a positive slope peak to be recorded as a rising edge, relative to the overall maximum negative slope.",
-        type=float,
-        default=0.3,
-    )
-    argument_parser.add_argument(
-        "--output-downsampled-slope-file",
-        help="(Only useful for debugging) Write the first derivative (slope) of the downsampled recording as a WAV file to the given path",
+        "--output-highpassed-file",
+        help="(Only useful for debugging) Write the highpassed recording as a WAV file to the given path",
         type=argparse.FileType(mode="wb"),
     )
     argument_parser.add_argument(
@@ -98,13 +91,13 @@ def parse_arguments():
         type=argparse.FileType(mode="wb"),
     )
     argument_parser.add_argument(
-        "--output-trimmed-slope-file",
+        "--output-trimmed-file",
         help="(Only useful for debugging) Write the trimmed recording as a WAV file to the given path",
         type=argparse.FileType(mode="wb"),
     )
     argument_parser.add_argument(
-        "--output-upsampled-slope-file",
-        help="(Only useful for debugging) Write the recording slope as a WAV file to the given path",
+        "--output-zero-crossing-slopes-file",
+        help="(Only useful for debugging) Write the slopes at zero crossings as a WAV file to the given path",
         type=argparse.FileType(mode="wb"),
     )
     argument_parser.add_argument(
@@ -124,58 +117,6 @@ def generate_boundaries_reference_samples(frame_count, fps_num, fps_den, sample_
     )
 
 
-def find_edges(
-    slope,
-    minimum_negative_height,
-    minimum_positive_height,
-    minimum_falling_edge_distance,
-    minimum_rising_edge_distance,
-    minimum_negative_prominence,
-    minimum_positive_prominence,
-):
-    """Looks for edges in the recording slope signal.
-
-    Returns a Series whose index is the offset into `slope` and whose values
-    are booleans indicating a falling edge (False) or a rising edge (True).
-    """
-    falling_edge_indexes = scipy.signal.find_peaks(
-        -slope,
-        height=minimum_negative_height,
-        distance=minimum_falling_edge_distance,
-        prominence=minimum_negative_prominence,
-    )[0]
-    rising_edge_indexes = scipy.signal.find_peaks(
-        slope,
-        height=minimum_positive_height,
-        distance=minimum_rising_edge_distance,
-        prominence=minimum_positive_prominence,
-    )[0]
-    return pd.Series(
-        np.concatenate(
-            [
-                np.repeat(False, falling_edge_indexes.size),
-                np.repeat(True, rising_edge_indexes.size),
-            ]
-        ),
-        index=pd.Index(
-            np.concatenate([falling_edge_indexes, rising_edge_indexes]), name="offset"
-        ),
-        name="frame",
-    ).sort_index()
-
-
-def generate_downsampling_derivative_kernel(downsampling_ratio):
-    return scipy.signal.convolve(
-        scipy.signal.firwin(
-            numtaps=2 * int(2 * 10 * downsampling_ratio) + 1,
-            cutoff=1 / downsampling_ratio,
-        ),
-        # Take first derivative (differentiation). See
-        # https://terpconnect.umd.edu/~toh/spectrum/Convolution.html
-        [1, -1],
-    )
-
-
 def analyze_recording():
     args = parse_arguments()
     assert (
@@ -183,8 +124,9 @@ def analyze_recording():
     ), "The number of frames in the boundaries reference signal should be odd so that the signal begins and ends on the same frame"
     spec = json.load(args.spec_file)
     nominal_fps = spec["fps"]["num"] / spec["fps"]["den"]
+    expected_transition_count = spec["transition_count"]
     frames = videojitter.util.generate_frames(
-        spec["transition_count"], spec["delayed_transitions"]
+        expected_transition_count, spec["delayed_transitions"]
     )
     reference_duration_seconds = len(frames) / nominal_fps
     print(
@@ -213,26 +155,61 @@ def analyze_recording():
             file, int(recording_sample_rate), samples.astype(np.float32)
         )
 
-    # A rising edge is always followed by a falling edge, so the period is twice
-    # the distance between edges. Therefore, in order to resolve a given edge
-    # interval, we need to be able to resolve a signal of a frequency that is
-    # half the edge frequency. The minimum sample rate is double that (Nyquist),
-    # so these cancel each other out.
+    # If we assume the worst-case scenario where all the edges in the input
+    # signal are separated by the min threshold Tm, then the fundamental
+    # period in the input signal is 2*Tm (because the period is a full rising
+    # edge + falling edge cycle), and the fundamental frequency is 1/(2*Tm).
+    # However we also need to preserve the second harmonic, because that carries
+    # the information about waveform asymmetry - if we lose that, then it will
+    # look like every rising edge occurs exactly halfway between every falling
+    # edge (and vice-versa), thus destroying important timing information that
+    # the user may care about (consider e.g. 3:2 patterns). Therefore we need to
+    # preserve frequencies up to 1/Tm, and thus, per Nyquist, we need a sampling
+    # rate of at least 2/Tm.
     downsampling_ratio = np.floor(
-        recording_sample_rate / (1 / args.min_edge_separation_seconds)
+        0.5 * args.min_edge_separation_seconds * recording_sample_rate
     )
     recording_sample_rate /= downsampling_ratio
     print(
         f"Downsampling recording by {downsampling_ratio}x (to {recording_sample_rate} Hz)",
         file=sys.stderr,
     )
-    recording_slope = scipy.signal.resample_poly(
+    recording_samples = scipy.signal.resample_poly(
         recording_samples,
         up=1,
         down=downsampling_ratio,
-        window=generate_downsampling_derivative_kernel(downsampling_ratio),
     )
-    maybe_write_wavfile(args.output_downsampled_slope_file, recording_slope)
+    maybe_write_wavfile(args.output_downsampled_file, recording_samples)
+
+    # If we asssume the worst-case scenario where the input signal is a
+    # perfectly smooth (in other words, perfectly sluggish) sinusoid where each
+    # edge is separated by the max threshold, Tm, then the only period in the
+    # input signal is the fundamental period which is 2*Tm (because a period is
+    # a rising edge followed by a falling edge), and the fundamental frequency
+    # is 1/(2*Tm), which is the minimum frequency we need to preserve.
+    min_frequency = 0.5 / args.max_edge_spread_seconds
+    print(
+        f"Removing frequencies lower than {min_frequency} Hz from the recording",
+        file=sys.stderr,
+    )
+    # Note: the default filter impulse response rings in the time domain,
+    # resulting in spurious zero crossings that weren't there in the original
+    # signal. This is probably fine though, because it's ringing at the cutoff
+    # frequency, which is low enough that it wouldn't produce a steep zero
+    # crossing that could be mistaken for an edge. If we really want to get rid
+    # of these, one idea could be to use a Gaussian or raised cosine filter
+    # instead.
+    recording_samples = scipy.signal.convolve(
+        recording_samples,
+        scipy.signal.firwin(
+            int(np.ceil(10 * recording_sample_rate / min_frequency / 2)) * 2 + 1,
+            min_frequency,
+            pass_zero=False,
+            fs=recording_sample_rate,
+        ),
+        "same",
+    )
+    maybe_write_wavfile(args.output_highpassed_file, recording_samples)
 
     boundaries_reference_samples = generate_boundaries_reference_samples(
         args.boundaries_signal_frames,
@@ -245,7 +222,7 @@ def analyze_recording():
     )
 
     cross_correlation = scipy.signal.correlate(
-        recording_slope, boundaries_reference_samples, mode="valid"
+        recording_samples, boundaries_reference_samples, mode="valid"
     )
     maybe_write_wavfile(
         args.output_cross_correlation_file,
@@ -274,60 +251,73 @@ def analyze_recording():
         file=sys.stderr,
     )
 
-    recording_slope = recording_slope[test_signal_start_index:test_signal_end_index]
-    maybe_write_wavfile(args.output_trimmed_slope_file, recording_slope)
+    recording_samples = recording_samples[test_signal_start_index:test_signal_end_index]
+    maybe_write_wavfile(args.output_trimmed_file, recording_samples)
 
-    upsampling_ratio = np.ceil(
-        (1 / args.timestamp_resolution_seconds) / recording_sample_rate
+    zero_crossing_indexes = np.nonzero(np.diff(recording_samples > 0))[0]
+    zero_crossing_slopes = np.diff(recording_samples)[zero_crossing_indexes]
+    if args.output_zero_crossing_slopes_file:
+        recording_zero_crossing_slopes = np.zeros(recording_samples.size)
+        recording_zero_crossing_slopes[zero_crossing_indexes] = zero_crossing_slopes
+        maybe_write_wavfile(
+            args.output_zero_crossing_slopes_file, recording_zero_crossing_slopes
+        )
+    zero_crossing_absolute_slopes = np.abs(zero_crossing_slopes)
+
+    # Noise in the recording signal can result in lots of spurious zero
+    # crossings, especially if the last frame transition happened some time ago
+    # and the signal is now back to hovering around zero. We need a way to tell
+    # the spurious zero crossings apart from the true edges. The solution we use
+    # here relies on the signal slope at the zero crossing as the criterion. We
+    # need to decide on a slope threshold below which a zero crossing will be
+    # rejected. The correct threshold must be below the minimum true edge slope,
+    # which depends on overall signal shape and amplitude, so we'll have to take
+    # a guess. We could reference our guess on the steepest slope among all zero
+    # crossings in the signal - that's pretty much guaranteed to be a valid
+    # edge - but that would make the threshold very sensitive to an isolated
+    # outlier. To avoid this problem we base our guess on the Nth steepest
+    # slope, where N is large enough to mitigate the influence of outliers, but
+    # small enough that we can be reasonably confident we're still going to pick
+    # a valid edge even if the test signal contains fewer edges than expected.
+    # We then multiply that reference with a fudge factor to allow for edges
+    # with slightly smaller slopes, and that's our threshold.
+    zero_crossing_partition_nth = int(
+        np.round(expected_transition_count * args.min_edges_ratio)
     )
-    recording_sample_rate *= upsampling_ratio
-    test_signal_start_index *= upsampling_ratio
-    test_signal_end_index *= upsampling_ratio
+    edge_slope_reference = np.partition(
+        zero_crossing_absolute_slopes, -zero_crossing_partition_nth
+    )[-zero_crossing_partition_nth]
+    edge_slope_threshold = edge_slope_reference * args.edge_slope_threshold
+    valid_edge = zero_crossing_absolute_slopes > edge_slope_threshold
+    valid_edge_indexes = zero_crossing_indexes[valid_edge]
     print(
-        f"Upsampling recording slope by {upsampling_ratio}x to {recording_sample_rate} Hz",
+        f"{zero_crossing_partition_nth}nth steepest absolute zero crossing slope is {edge_slope_reference}. Kept {valid_edge_indexes.size} edges (out of {zero_crossing_indexes.size} zero crossings) whose absolute slope is above {edge_slope_threshold}. First edge is right after {format_index(valid_edge_indexes[0])} and last edge is right after {format_index(valid_edge_indexes[-1])}.",
         file=sys.stderr,
     )
-    recording_slope = scipy.signal.resample_poly(
-        recording_slope,
-        up=upsampling_ratio,
-        down=1,
-    )
-    maybe_write_wavfile(args.output_upsampled_slope_file, recording_slope)
-
-    recording_slope_min = recording_slope.min()
-    recording_slope_max = recording_slope.max()
-    print(
-        f"Recording slope range: [{recording_slope_min}, {recording_slope_max}]",
-        file=sys.stderr,
-    )
-
-    minimum_edge_distance_samples = int(
-        args.min_edge_separation_seconds * 2 * recording_sample_rate
-    )
-    find_edge_params = {
-        "minimum_negative_height": -recording_slope_min
-        * args.minimum_negative_slope_peak_height_ratio,
-        "minimum_positive_height": recording_slope_max
-        * args.minimum_positive_slope_peak_height_ratio,
-        "minimum_falling_edge_distance": minimum_edge_distance_samples,
-        "minimum_rising_edge_distance": minimum_edge_distance_samples,
-        "minimum_negative_prominence": -recording_slope_min
-        * args.minimum_negative_slope_peak_prominence_ratio,
-        "minimum_positive_prominence": recording_slope_max
-        * args.minimum_positive_slope_peak_prominence_ratio,
-    }
-    print(
-        f"Finding edges with parameters: {find_edge_params}",
-        file=sys.stderr,
-    )
-    edges = find_edges(recording_slope, **find_edge_params)
-    print(f"Detected {edges.index.size} edges (frame transitions).", file=sys.stderr)
-    assert edges.index.size > 0
-
+    assert valid_edge_indexes.size > 0
+    valid_edge_is_rising = zero_crossing_slopes[valid_edge] > 0
     if args.output_edges_file:
-        is_edge = np.zeros(recording_slope.size)
-        is_edge[edges.index] = edges * 2 - 1
-        maybe_write_wavfile(args.output_edges_file, is_edge)
+        recording_edges = np.zeros(recording_samples.size)
+        recording_edges[valid_edge_indexes] = valid_edge_is_rising * 2 - 1
+        maybe_write_wavfile(args.output_edges_file, recording_edges)
+
+    edges = pd.Series(
+        valid_edge_is_rising,
+        index=pd.Index(
+            # TODO: the precision of these timestamps is limited by the sample
+            # rate, which is quite low. We could compute a more precise estimate
+            # using linear interpolation from both sides of the zero crossing,
+            # but that's not ideal either, especially as the signal frequency
+            # approaches the sample rate (Svitlov, Sergiy et al. “Accuracy
+            # assessment of the two-sample zero-crossing detection in a
+            # sinusoidal signal.” Metrologia 49 (2012): 413 - 424). We could
+            # either upsample and linearly interpolate, or use more
+            # sophisticated interpolation techniques such as cubic or splines.
+            (valid_edge_indexes + test_signal_start_index) / recording_sample_rate,
+            name="recording_timestamp_seconds",
+        ),
+        name="frame",
+    )
 
     first_edge = edges.iloc[0]
     last_edge = edges.iloc[-1]
@@ -344,8 +334,7 @@ def analyze_recording():
         if not first_edge:
             edges = ~edges
 
-    edges.index = (edges.index + test_signal_start_index) / recording_sample_rate
-    edges.rename_axis("recording_timestamp_seconds").to_csv(sys.stdout)
+    edges.to_csv(sys.stdout)
 
 
 analyze_recording()
