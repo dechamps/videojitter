09c319dff5b019881aa77976646ea1c0ecb5a1ff
Etienne Dechamps <etienne@edechamps.fr>
2023-10-08 20:27:31 +0100

    Add utility for running test pipeline

--- videojitter/tests/fake/__init__.py
+++ videojitter/tests/fake/__init__.py
@@ -1,53 +1,7 @@
 import sys
 
+import videojitter.testing
+
 
 async def videojitter_test(test_case):
-    spec_path = test_case.get_output_path("spec.json")
-    await test_case.run_subprocess(
-        "generate_spec",
-        sys.executable,
-        "-m",
-        "videojitter.generate_spec",
-        "--output-spec-file",
-        spec_path,
-    )
-    recording_path = test_case.get_output_path("recording.wav")
-    await test_case.run_subprocess(
-        "generate_fake_recording",
-        sys.executable,
-        "-m",
-        "videojitter.generate_fake_recording",
-        "--spec-file",
-        spec_path,
-        "--output-recording-file",
-        recording_path,
-    )
-    frame_transitions_csv_path = test_case.get_output_path("frame_transitions.csv")
-    await test_case.run_subprocess(
-        "analyze_recording",
-        sys.executable,
-        "-m",
-        "videojitter.analyze_recording",
-        "--spec-file",
-        spec_path,
-        "--recording-file",
-        recording_path,
-        "--output-frame-transitions-csv-file",
-        frame_transitions_csv_path,
-    )
-    report_csv_path = test_case.get_output_path("report.csv")
-    report_chart_path = test_case.get_output_path("report.json")
-    await test_case.run_subprocess(
-        "generate_report",
-        sys.executable,
-        "-m",
-        "videojitter.generate_report",
-        "--spec-file",
-        spec_path,
-        "--frame-transitions-csv-file",
-        frame_transitions_csv_path,
-        "--output-csv-file",
-        report_csv_path,
-        "--output-chart-file",
-        report_chart_path,
-    )
+    await videojitter.testing.run_pipeline(test_case)
