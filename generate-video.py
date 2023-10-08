8beaa692f86e8d4d0b6f37259ea5646bde805de5
Etienne Dechamps <etienne@edechamps.fr>
2023-10-08 10:54:34 +0100

    Print ffmpeg command line

--- generate-video.py
+++ generate-video.py
@@ -36,47 +36,46 @@ def generate_video():
     args = parse_arguments()
     spec = json.load(sys.stdin)
 
-    ffmpeg_process = (
-        ffmpeg.output(
-            ffmpeg.input(
-                "pipe:",
-                format="rawvideo",
-                pix_fmt="gray",
-                s="1x1",
-                r=f"{spec['fps']['num']}/{spec['fps']['den']}",
-            ).filter(
-                "tpad",
-                start_duration=args.begin_padding,
-                stop_duration=args.end_padding,
-                start_mode="clone",
-                stop_mode="clone",
-            ),
-            # Include a dummy audio track as it makes the test video more
-            # realistic. Some video players (especially PC software) rely on the
-            # audio track for clocking, and will behave very differently if it's
-            # not there.
-            ffmpeg.input("anoisesrc=c=pink:r=48000:a=0.001", format="lavfi"),
-            args.output_file,
-            **{
-                "shortest": None,
-                "profile:v": "baseline",
-                "preset": "ultrafast",
-                # Make the video behave like typical HD video for compatibility
-                # and to ensure the video players behave similarly to a "real"
-                # video.
-                "s": "hd1080",
-                "pix_fmt": "yuv420p",
-                "color_primaries": "bt709",
-                "color_trc": "bt709",
-                "colorspace": "bt709",
-                "color_range": "tv",
-                "acodec": "ac3",
-            },
-        )
-        .overwrite_output()
-        .run_async(pipe_stdin=True)
-    )
+    ffmpeg_spec = ffmpeg.output(
+        ffmpeg.input(
+            "pipe:",
+            format="rawvideo",
+            pix_fmt="gray",
+            s="1x1",
+            r=f"{spec['fps']['num']}/{spec['fps']['den']}",
+        ).filter(
+            "tpad",
+            start_duration=args.begin_padding,
+            stop_duration=args.end_padding,
+            start_mode="clone",
+            stop_mode="clone",
+        ),
+        # Include a dummy audio track as it makes the test video more
+        # realistic. Some video players (especially PC software) rely on the
+        # audio track for clocking, and will behave very differently if it's
+        # not there.
+        ffmpeg.input("anoisesrc=c=pink:r=48000:a=0.001", format="lavfi"),
+        args.output_file,
+        **{
+            "shortest": None,
+            "profile:v": "baseline",
+            "preset": "ultrafast",
+            # Make the video behave like typical HD video for compatibility
+            # and to ensure the video players behave similarly to a "real"
+            # video.
+            "s": "hd1080",
+            "pix_fmt": "yuv420p",
+            "color_primaries": "bt709",
+            "color_trc": "bt709",
+            "colorspace": "bt709",
+            "color_range": "tv",
+            "acodec": "ac3",
+        },
+    ).overwrite_output()
 
+    print(ffmpeg_spec.compile(), file=sys.stderr)
+
+    ffmpeg_process = ffmpeg_spec.run_async(pipe_stdin=True)
     ffmpeg_process.stdin.write(
         (
             videojitter.util.generate_frames(
