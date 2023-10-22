72d5e7b0884ff43a71275fe2213dfbfb3cd3060c
Etienne Dechamps <etienne@edechamps.fr>
2023-10-22 10:37:30 +0100

    Add LG 119p black/white padding tests

---
+++ videojitter/tests/lg_119p_whitepadding/__init__.py
@@ -0,0 +1,7 @@
+import videojitter.testing
+
+
+async def videojitter_test(test_case):
+    with videojitter.testing.Pipeline(test_case) as pipeline:
+        await pipeline.run_analyze_recording()
+        await pipeline.run_generate_report()
