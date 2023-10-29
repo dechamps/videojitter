dbe5393633b9017f589ff8a1cbc0e07e423b146a
Etienne Dechamps <etienne@edechamps.fr>
2023-10-29 18:15:32 +0000

    Add a high amplitude test

---
+++ videojitter/tests/high_amplitude/__init__.py
@@ -0,0 +1,9 @@
+import videojitter.testing
+
+
+async def videojitter_test(test_case):
+    with videojitter.testing.Pipeline(test_case) as pipeline:
+        await pipeline.run_generate_spec()
+        await pipeline.run_generate_fake_recording("--amplitude", 2)
+        await pipeline.run_analyze_recording()
+        await pipeline.run_generate_report()
