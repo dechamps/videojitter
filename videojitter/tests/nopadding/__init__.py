c42e644125b55d56128ffe8aeeec30378d0b54f6
Etienne Dechamps <etienne@edechamps.fr>
2023-10-29 17:41:54 +0000

    Add test with no padding

---
+++ videojitter/tests/nopadding/__init__.py
@@ -0,0 +1,13 @@
+import videojitter.testing
+
+
+async def videojitter_test(test_case):
+    with videojitter.testing.Pipeline(test_case) as pipeline:
+        await pipeline.run_generate_spec()
+        await pipeline.run_generate_fake_recording(
+            "--begin-padding-seconds", -0.030, "--end-padding-seconds", -0.030
+        )
+        await pipeline.run_analyze_recording()
+        await pipeline.run_generate_report(
+            "--keep-first-transition", "--keep-last-transition"
+        )
