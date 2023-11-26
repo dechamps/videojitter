324e02194c61fb5c39686afa5c33b661d913b7d8
Etienne Dechamps <etienne@edechamps.fr>
2023-11-26 23:07:31 +0000

    Run multiple reports in parallel

--- videojitter_test/cases/asuswmp_240p_at_240hz/__init__.py
+++ videojitter_test/cases/asuswmp_240p_at_240hz/__init__.py
@@ -1,14 +1,18 @@
+import asyncio
+
 from videojitter_test import _pipeline
 
 
 async def videojitter_test(test_case):
     with _pipeline.Pipeline(test_case) as pipeline:
         await pipeline.run_analyze_recording()
-        await pipeline.run_generate_report()
-        await pipeline.run_generate_report(
-            "--chart-start-seconds",
-            "34.83",
-            "--chart-end-seconds",
-            "35.06",
-            prefix="zoomed_",
+        await asyncio.gather(
+            pipeline.run_generate_report(),
+            pipeline.run_generate_report(
+                "--chart-start-seconds",
+                "34.83",
+                "--chart-end-seconds",
+                "35.06",
+                prefix="zoomed_",
+            ),
         )
--- videojitter_test/cases/lg_23p/__init__.py
+++ videojitter_test/cases/lg_23p/__init__.py
@@ -1,14 +1,18 @@
+import asyncio
+
 from videojitter_test import _pipeline
 
 
 async def videojitter_test(test_case):
     with _pipeline.Pipeline(test_case) as pipeline:
         await pipeline.run_analyze_recording()
-        await pipeline.run_generate_report()
-        await pipeline.run_generate_report(
-            "--chart-start-seconds",
-            "34",
-            "--chart-end-seconds",
-            "38",
-            prefix="zoomed_",
+        await asyncio.gather(
+            pipeline.run_generate_report(),
+            pipeline.run_generate_report(
+                "--chart-start-seconds",
+                "34",
+                "--chart-end-seconds",
+                "38",
+                prefix="zoomed_",
+            ),
         )
